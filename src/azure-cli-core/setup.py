c45a3cd33f4a24941a148bb4327a40106c390ea2
Martin Strobel <marstr@micrsoft.com>
2019-07-11 15:31:41 -0700

    Update package that provides msal-extensions

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -61,7 +61,7 @@ DEPENDENCIES = [
     'humanfriendly~=4.7',
     'jmespath',
     'knack~=0.6.2',
-    'marstr-msal-extensions~=0.0.1',
+    'msal-extensions~=0.1.0',
     'msrest>=0.4.4',
     'msrestazure>=0.4.25',
     'paramiko>=2.0.8,<3.0.0',
